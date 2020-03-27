15b0fc4c78eb151e3d3de7aeb322120f749b512c
Diana Utan <didiutzi@gmail.com>
2020-03-27 11:49:37 -0500

    java data types

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum value = " + myMinIntValue);
+        System.out.println("Integer Maximum value = " + myMaxIntValue);
+        System.out.println("Busted Max value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min value = " + (myMinIntValue - 1));
+    }
+}
