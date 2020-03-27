d0ccfc762ca8e50d813e7310fa3a16ce3b1dfd97
Diana Utan <didiutzi@gmail.com>
2020-03-27 11:26:03 -0500

    variables and expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -10,5 +10,10 @@ public class Hello {
 
     public static void main (String[] args) {
         System.out.println("Hello Diana");
+
+        int myFirstNumber = (10 + 5) + ( 2 * 10);
+
+        System.out.println(myFirstNumber);
+
     }
 }
