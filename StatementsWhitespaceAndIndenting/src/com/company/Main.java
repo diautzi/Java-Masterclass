095b7a3d22d4269a32bbfc964227e9ee8d93eaf5
Diana Utan <didiutzi@gmail.com>
2020-03-29 15:28:29 -0500

    statements and whitespaces in java

---
+++ StatementsWhitespaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // semicolon closes/finishes the statement
+        //statements can be on sevaral lines
+
+        int myVariable = 50; /// whole line is a Java statement
+        myVariable++; // also valid statement
+        myVariable--; //statement
+        System.out.println("Thsi is a test"); // also a statement
+
+        System.out.println("this is " +
+                "another " +
+                "statement");
+
+        int anotherStatement; myVariable++; //also valid statement
+
+        int myVar = 50; //sama as
+        int        myVar        = 50; // Java ignores extra whitespace
+    }
+}
