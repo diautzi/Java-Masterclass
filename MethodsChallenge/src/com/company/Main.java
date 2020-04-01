58218c28eb4e18ec4a21021d0c875c85eb7d41a6
Diana Utan <didiutzi@gmail.com>
2020-04-01 11:05:58 -0500

    second solution for methods challenge

--- MethodsChallenge/src/com/company/Main.java
+++ MethodsChallenge/src/com/company/Main.java
@@ -44,24 +44,37 @@ public class Main {
 
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
-        System.out.println(playerName + " managed to get into position" + highScorePosition + " on the high score table");
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table");
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+        ////////// first solution////////
+//
+//            if(playerScore >= 1000)
+//            {
+//                return 1;
+//            }
+//            else if (playerScore >=500) {
+//                return  2;
+//            }
+//            else if (playerScore >=100 ) {
+//                return  3;
+//            }
+//
+//            return 4;
+//
+//      ////// second solution//////////
 
-            if(playerScore >= 1000)
-            {
-                return 1;
-            }
-            else if (playerScore >=500 && playerScore < 1000) {
-                return  2;
-            }
-            else if (playerScore >=100 && playerScore < 500) {
-                return  3;
-            }
-            else {
-                return 4;
-            }
+        
+        int position = 4;
+        if(playerScore >= 1000) {
+            position = 1;
+        } else if ( playerScore >= 500) {
+            position = 2;
+        } else if ( playerScore >= 100) {
+            position = 3;
+        }
+        return position;
     }
 
 }
