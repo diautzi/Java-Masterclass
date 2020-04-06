08f0bfc4ac0e1f583915cf7544ee7193203f932e
Diana Utan <didiutzi@gmail.com>
2020-04-06 16:45:21 -0500

    equal sum checker

---
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int firstNum, int secondNum, int thirdNum) {
+
+        if ( firstNum + secondNum == thirdNum ) {
+            return true;
+        }
+        return false;
+    }
+}
