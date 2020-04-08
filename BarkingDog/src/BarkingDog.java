abcc6c2b9cd44e5c0052852c3975506c6b482dfb
Diana Utan <didiutzi@gmail.com>
2020-04-07 20:57:12 -0500

    Update BarkingDog.java

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -1,29 +1,13 @@
+
 public class BarkingDog {
 
-    public static boolean bark(boolean barking, int hourOfDay){
-        boolean result = false;
-        if (hourOfDay < 0 || hourOfDay > 23){
-            System.out.println("1st Condition");
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay>23) {
             return false;
         }
-        else if (hourOfDay >= 8 && hourOfDay <= 22){
-            System.out.println("2nd Condition");
-            result = false;
+        if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
         }
-
-        else if (hourOfDay >= 1 || hourOfDay <=7 || hourOfDay >=23){
-            if(barking == true){
-                System.out.println("3rd Condition");
-                result = true;
-            }
-            else {
-                System.out.println("4th Condition");
-                result = false;
-            }
-        }
-        return result;
-
-
+        return false;
     }
-
 }
