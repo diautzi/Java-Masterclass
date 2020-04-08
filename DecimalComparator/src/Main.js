6054f963336a6a02127c12b8ff6d86870191f136
Diana Utan <didiutzi@gmail.com>
2020-04-07 21:04:28 -0500

    Create Main.js

---
+++ DecimalComparator/src/Main.js
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+    
+        boolean threeDec = DecimalComparator.areEqualByThreeDecimalPlaces(-3.987, -3.987);
+        System.out.println(threeDec);
+    }
+
+}
