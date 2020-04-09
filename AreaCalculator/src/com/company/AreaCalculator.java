6d869346990f0f536590392995fe7ef3c0366f99
Diana Utan <didiutzi@gmail.com>
2020-04-09 16:38:40 -0500

    challenge requirements added

--- AreaCalculator/src/com/company/AreaCalculator.java
+++ AreaCalculator/src/com/company/AreaCalculator.java
@@ -2,6 +2,13 @@ package com.company;
 
 public class AreaCalculator {
 
+    //write method named area with one double param
+    //The method returns a double value trat represents the area of a circle
+    //if param radius is negatibve return -1
+    //write another overloaded method with 2 params x and y
+    //the method returns an area of a rectangle
+    //if either or both params are negative,return -1
+
     public static double area(double radius) {
 
         if( radius < 0 ) {
