7c73252a909e1d5f81f8bfabe3cde8d8690d5218
Diana Utan <didiutzi@gmail.com>
2020-04-10 21:32:03 -0500

    day of the week challenge

---
+++ DayOftheWeekChallenge/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        DayOfTheWeek.printDayOfTheWeek(1);
+        DayOfTheWeek.printDayOfTheWeek2(5);
+    }
+}
