af6ec8bff56ead09adfd634b9fcc3dda75328ac4
Diana Utan <didiutzi@gmail.com>
2020-04-10 21:45:55 -0500

    NumberInDays exercise

--- NumberInWord/src/com/company/Main.java
+++ NumberInWord/src/com/company/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-
+        NumberInWord.printNumberInWord(-1);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(3);
+        NumberInWord.printNumberInWord(4);
+        NumberInWord.printNumberInWord(5);
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(7);
+        NumberInWord.printNumberInWord(8);
+        NumberInWord.printNumberInWord(9);
+        NumberInWord.printNumberInWord(10);
     }
 }
