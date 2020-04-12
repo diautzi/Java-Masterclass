79608c7717eebcb37668e4d49d85908753811a9f
Diana Utan <didiutzi@gmail.com>
2020-04-12 18:33:35 -0500

    for loops in java

---
+++ TheForStatement/src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 // write your code here
+
+        System.out.println("10000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+
+        //// for loop in java ///////
+        for( int i=1; i < 5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
+
+        //using the for loop call the calculateInterest method with
+        //the amount of 10000 with an interest of 2,3,4,5,6,7 and 8
+        // and print thr result to the console window
+
+        System.out.println("Calculating interest starting at 2% up to 8%");
+
+        for( int i = 2; i <= 8; i++) {
+            System.out.println("10000 at + " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        //now start calculating at 8% and going down to 2%
+
+        System.out.println("Calculating interest starting at 8% down to 2%");
+
+        for ( int i=8; i >=2; i--) {
+            System.out.println("10000 at + " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
+        }
+
+        ///////testing isPrime method//////
+        
+        System.out.println(isPrime(2));
+
+        // Create a for statement using any range of numbers
+        // Determine if the number is a prime number using the isPrime method
+        // if it is a prime number, print it out AND increment a count of the
+        // number of prime numbers found
+        // if that count is 3 exit the for loop
+        // hint:  Use the break; statement to exit
+
+        int count = 0;
+        for(int i = 2; i < 30; i++) {
+            if(isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count == 3) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i=2; i <= n/2; i++) {
+            System.out.println("Looping " +i);
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static double calculateInterest( double amount, double interestRate) {
+        return ( amount * ( interestRate/100));
+    }
+
+}
