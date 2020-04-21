18b2a41908bfa06bfe8cc71e75ba9808456e3b74
Diana Utan <didiutzi@gmail.com>
2020-04-21 12:47:52 -0500

    second overloaded getBucketCount, that uses 3 params of typoe double and returns count of buckets needed for paint job of type int

--- PaintJob/src/com/company/Main.java
+++ PaintJob/src/com/company/Main.java
@@ -8,6 +8,9 @@ public class Main {
         System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5, 2));
         System.out.println(PaintJob.getBucketCount(2.75, 3.25, 2.5, 1));
 
-
+        System.out.println(PaintJob.getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(PaintJob.getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(PaintJob.getBucketCount(7.25, 4.3, 2.35));
+        
     }
 }
--- PaintJob/src/com/company/PaintJob.java
+++ PaintJob/src/com/company/PaintJob.java
@@ -2,23 +2,36 @@ package com.company;
 
 public class PaintJob {
 
+    //1.
     //write method getBucketCount eint 4 params
     // 1st param => width( double)
     //2nd => height (double)
     //3rd => areaPerBucket = area that can be covered with one buccket of paint
     //4th => extraBuckets = buckets at home
     //method returns the nr of buckets that Bob needs to buy before going to work
-    // if one of the first 3 params < 0 => return -1
+    // if one of the first 3 params <= 0  or extraBuckets < 0 => return -1
 
 
     public static int getBucketCount( double width, double height, double areaPerBucket, double extraBuckets) {
 
-        if ( width > 0 && height > 0 && areaPerBucket > 0 ) {
+        if ( width > 0 && height > 0 && areaPerBucket > 0 && areaPerBucket >= 0) {
             double area = width * height;
             return (int) Math.ceil(area / areaPerBucket - extraBuckets);
         }
         return -1;
     }
 
-    
+
+    //2.
+    //Bob does not like to enter 0 for extraBuckets, so:
+    //write a second overloaded method named getBucketCount with 3 params (width, height, areaPerBucket)
+    // returns a value of type int that represents the number of buckets needed for the paint job
+
+    public static int getBucketCount( double width, double height, double areaPerBucket) {
+        if ( width > 0 && height > 0 && areaPerBucket > 0) {
+            return (int) Math.ceil(( width * height / areaPerBucket ));
+        }
+        return -1;
+    }
+
 }
