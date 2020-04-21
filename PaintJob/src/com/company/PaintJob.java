6f5b93d2ae570549dc243c0d257cf2d1e1728bbf
Diana Utan <didiutzi@gmail.com>
2020-04-21 13:18:45 -0500

    thrird overloaded getBucketCount method, that uses 2 params of type double and returns count of buckets needed for paint job of type int

--- PaintJob/src/com/company/PaintJob.java
+++ PaintJob/src/com/company/PaintJob.java
@@ -12,9 +12,9 @@ public class PaintJob {
     // if one of the first 3 params <= 0  or extraBuckets < 0 => return -1
 
 
-    public static int getBucketCount( double width, double height, double areaPerBucket, double extraBuckets) {
+    public static int getBucketCount( double width, double height, double areaPerBucket, int extraBuckets) {
 
-        if ( width > 0 && height > 0 && areaPerBucket > 0 && areaPerBucket >= 0) {
+        if ( width > 0 && height > 0 && areaPerBucket > 0 && areaPerBucket >= 0 && extraBuckets >= 0) {
             double area = width * height;
             return (int) Math.ceil(area / areaPerBucket - extraBuckets);
         }
